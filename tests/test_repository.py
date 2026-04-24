dd53b9e744170a9a69ca6dcc13d10c2d7f4e2f7b
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-04-24 21:00:03 +0200

    test: add change user land functionality test

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -3,6 +3,7 @@ from dld_quiz_bot.db import repository as rp
 TELEGRAM_ID = 123456789
 USERNAME = "test_user"
 SELECTED_LAND = "Bayern"
+NEW_LAND = "Berlin"
 
 
 async def test_create_user(pool):
@@ -21,3 +22,10 @@ async def test_get_user(pool):
 async def test_get_user_not_found(pool):
     user = await rp.get_user(pool, 999999999)
     assert user is None
+
+
+async def test_change_user_land(pool):
+    await rp.create_user(pool, TELEGRAM_ID, USERNAME, SELECTED_LAND)
+    await rp.change_user_land(pool, TELEGRAM_ID, NEW_LAND)
+    user = await rp.get_user(pool, TELEGRAM_ID)
+    assert user.selected_land == NEW_LAND
