cc57e558d06e2fb608975b59f018134bb39550c4
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-04-27 14:39:43 +0200

    feat: update db migration to fill questions table

--- alembic/versions/0627b8ad4ba2_create_initial_tables.py
+++ alembic/versions/0627b8ad4ba2_create_initial_tables.py
@@ -6,7 +6,10 @@ Create Date: 2026-04-22 14:07:02.076145
 
 """
 
+import json
 from collections.abc import Sequence
+from pathlib import Path
+from typing import Any
 
 import sqlalchemy as sa
 
@@ -17,6 +20,12 @@ revision: str = "0627b8ad4ba2"
 down_revision: str | Sequence[str] | None = None
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
+
+
+def load_questions_from_json(filepath: Path) -> Any:
+    with open(filepath, encoding="utf-8") as f:
+        questions = json.load(f)
+    return questions
 
 
 def upgrade() -> None:
@@ -52,6 +61,25 @@ def upgrade() -> None:
         ),
         sa.ForeignKeyConstraint(["user_id"], ["users.telegram_id"], ondelete="CASCADE"),
     )
+
+    conn = op.get_bind()
+    PATH = Path(__file__).parent.parent.parent / "data" / "questions.json"
+    questions = load_questions_from_json(PATH)
+
+    for question in questions:
+        conn.execute(
+            sa.text("""
+                INSERT INTO questions (text, options, correct_answer, topic, land)
+                VALUES (:text, :options, :correct_answer, :topic, :land)
+            """),
+            {
+                "text": question["text"],
+                "options": json.dumps(question["options"], ensure_ascii=False),
+                "correct_answer": question["correct_answer"],
+                "topic": question["topic"],
+                "land": question["land"],
+            }
+        )
 
 
 def downgrade() -> None:
